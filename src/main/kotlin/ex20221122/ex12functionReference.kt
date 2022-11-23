3fac8425d3665041d6a3e826c10f6f002b04249d
SUNGJUN1234 <chotjdwns654@naver.com>
2022-11-23 17:56:45 +0900

    lambda , higherOrderFunction

--- src/main/kotlin/ex20221122/ex12functionReference.kt
+++ src/main/kotlin/ex20221122/ex12functionReference.kt
@@ -5,7 +5,7 @@ fun todayWeather(){
 }
 // 함수를 변수에 대입하는 방법
 // ::함수이름 ---> 안드로이드 intent
-// ::(리플렉션)
+// ::(reflection)
 val weather = ::todayWeather
 
 
@@ -13,5 +13,6 @@ fun main() {
     // 함수를 참조해서 변수처럼 사용해보자
     // 함수 참조를 통해서 변수처럼 사용하고 싶으면
     // 변수명 뒤에 .call() 이라는 메서드를 붙여줘야한다
+    // weather.call() // 1.1version에서 사용하던 방법
     weather()
 }
